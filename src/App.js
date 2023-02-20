76e05301dd3d3da48fb11831d414f5f6af81a996
GabrielaMason <0202578@up.edu.mx>
2023-02-20 14:08:45 -0600

    Apply API request without Redux

--- src/App.js
+++ src/App.js
@@ -1,31 +1,66 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import CardsContainer from './components/showCards'
 import { getAll } from './services/cards';
 import { initCards } from './reducers/cardReducer';
 import { useDispatch } from 'react-redux';
+import axios from 'axios';
+import _ from "lodash"
 
 function App() {
   const dispatch = useDispatch()
+  const [cards, setCards] = useState([])
+  const [paginatedCards, setPaginatedCards] = useState()
+  const [currentPage, setCurrentPage] = useState(1)
+  const pageSize = 50;
 
   useEffect(() => {
       //inicializar el estado de la store con las cartas
-      getAll().then(cards => {
+      /*getAll().then(cards => {
         dispatch(initCards(cards))
-      })
-  }, [dispatch]) //el dispatch no cambia
+      })*/
+      axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php')
+        .then(res => {
+          console.log(res.data)
+          setCards(res.data.data)
+          setPaginatedCards(_(res.data.data).slice(0).take(pageSize).value())
+        })
+        .catch(err => {
+          console.log(err)
+        })
+  }, []) //el dispatch no cambia
 
-  const filterSelected = value => {
+  /*const filterSelected = value => {
     console.log(value)
+  }*/
+  const pageCount = cards ? Math.ceil(cards.length/pageSize) :0;
+
+  if (pageCount === 1) {
+    return null
+  }
+
+  const pages = _.range(1, pageCount+1)
+
+  const pagination = (pageNo) => {
+    setCurrentPage(pageNo);
+    const startIndex = (pageNo - 1) * pageSize;
+    const paginatedPost = _(cards).slice(startIndex).take(pageSize).value();
+    setPaginatedCards(paginatedPost)
   }
 
   return (
     <div className="App">
       <div>
-        all
-        <input type='radio' name='filter' onChange={() => filterSelected('ALL')}/>
+        {/*console.log("Tarjetas", cards.data)}
+        {cards.map(card => {
+          return(
+            <li key={card.id}>
+              {card.name}
+            </li>
+          )
+        })*/}
+        <CardsContainer cards={cards} pages={pages} paginatedCards={paginatedCards} currentPage={currentPage} pagination={pagination}/>
       </div>
-      <CardsContainer />
     </div>
   );
 }
--- src/components/showCards.js
+++ src/components/showCards.js
@@ -1,13 +1,13 @@
 import { useDispatch, useSelector } from "react-redux"
 import {toggleImportanceOf} from '../reducers/cardReducer'
 
-function Cards ({cards, toggleImportant}) {
+function Cards ({cards}) {
     return (<div className="cardsContainer">
-        {cards.map((card, index) => {
-            <div key={index} className="card mb-3 cardShadow" style={{maxWidth: '540px'}}>
+        {cards.map(card => {
+            return(<div key={card.id} className="card mb-3 cardShadow" style={{maxWidth: '540px'}}>
                 <div className="row g-0">
                     <div className="col-md-4">
-                        <img src={card.card_images.image_url} className="img-fluid rounded-start" alt="..." />
+                        <img src={card.card_images[0].image_url} className="img-fluid rounded-start" alt="..." />
                     </div>
                     <div className="col-md-8">
                         <div className="card-body">
@@ -16,26 +16,37 @@ function Cards ({cards, toggleImportant}) {
                                 <h5 className="card-title">{card.type}</h5>
                             </div>
                             <p className="card-text">{card.desc}</p>
-                            <p className="float-end">{card.card_prices.tcgplayer_price}</p>
+                            <p className="float-end">Precio: {card.card_prices[0].tcgplayer_price}</p>
                         </div>
                     </div>
                 </div>
         </div>
+            )
         })}
     </div>)
 }
 
-export default function CardsContainer() {
+export default function CardsContainer({cards, pages, paginatedCards, currentPage, pagination}) {
     //Obtener sólo la data de nuestro objeto para mostrar las cartas
-    const dispatch = useDispatch();
+    /*const dispatch = useDispatch();
     const cards = useSelector(state => state.cards.data);
     console.log("CARTAS: ", {cards})
 
     const toggleImportant = (id) => {
         dispatch(toggleImportanceOf(id))
-    }
+    }*/
 
-    return(
-        <Cards cards={cards} toggleImportant={toggleImportant} />
+    return(<>
+    {/*<nav className="d-flex justify-content-center">
+        <ul className="pagination">
+            {pages.map((page) => {
+                 return(<li className={page === currentPage ? "page-item active" : "page-item"}>
+                    <p className="page-link" onClick={()=>pagination(page)}>{page}</p>
+                </li>)
+            })}
+        </ul>
+        </nav>}*/}
+    <Cards cards={cards}/>
+    </>
     )
 }
